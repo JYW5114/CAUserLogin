d77aad2989f4a026a1f87fa61eb99356a78f4b2e
JYW5114 <elowenyiwei.jiang@mail.utoronto.ca>
2024-10-19 21:26:42 -0400

    task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name the name of user
      */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return name of user.
+     */
+    String getCurrentUser();
 }
